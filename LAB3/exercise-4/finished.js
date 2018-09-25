600bd98d3d50e67552275b58ebcd1cd9d2dbebd6
Tam Dang <danhtam0509@gmail.com>
2018-09-25 18:59:01 -0400

    Update Lab3- Exercise 4-part2

--- LAB3/exercise-4/finished.js
+++ LAB3/exercise-4/finished.js
@@ -2,9 +2,24 @@
 var max = 50;
 
 var handleCounter = function (err, counter, waitTime, timestamp) {
-    
+    if (err) console.log(err)
+    else {
+        console.log("Callback counter: " + counter + " Time: " + waitTime + " ms TimeStamp: " + timestamp);
+    }
 };
 
 var callbackWait = function(counter, callback) {
-    
+    var waitTime = Math.floor((Math.random()*60) + 1);
+    var timestamp = new Date();
+    if (waitTime > max) {
+       var error = Error("ERROR: WaitTime cannot be greater than Max Time");
+       callback(error.message, counter, waitTime, timestamp);
+    }
+    else {
+        setTimeout(callback, waitTime, "", counter, waitTime, timestamp.toUTCString());
+    }
+}
+
+for (var i=0; i < 10; i++) {
+    callbackWait(i, handleCounter);    
 }
