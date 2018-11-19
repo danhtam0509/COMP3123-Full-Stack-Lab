502821e20742d0a4cca4c21f1e091683e9921865
Tam Dang <danhtam0509@gmail.com>
2018-11-19 03:37:41 -0500

    Protected Requests & Auth Guard

--- meanauthapp/angular-src/src/app/services/auth.service.ts
+++ meanauthapp/angular-src/src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService} from '@auth0/angular-jwt';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -23,11 +25,35 @@ export class AuthService {
         .pipe(map(res => res.json()));
     }
 
+    getProfile() {
+      let headers = new Headers();
+      this.loadToken();
+      headers.append('Authorization', this.authToken);
+      headers.append('Content-Type', 'application/json');
+      return this.http.get('http://localhost:3000/users/profile', {headers: headers})
+        .pipe(map(res => res.json()));
+    }
+
     storeUserData(token, user) {
       localStorage.setItem('id_token', token);
       localStorage.setItem('user', JSON.stringify(user));
       this.authToken = token;
       this.user = user;
+    }
+
+    loadToken() {
+      const token = localStorage.getItem('id_token');
+      this.authToken = token;
+    }
+
+    loggedIn() {
+      if (localStorage.id_token == undefined) {
+        return false
+      } else {
+        const helper = new JwtHelperService();
+        return !helper.isTokenExpired(localStorage.id_token);
+      }
+     
     }
 
     logout() {
